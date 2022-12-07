1e270e04ddf5b151a926b48bb84746c76270cab6
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-07 09:53:17 +0300

    Comleted

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// code your solution here
+function saturdayFun(activity='roller-skate') {
+ 
+    return `This Saturday, I want to ${activity}!`;  
+    }
+    function mondayWork(activity='go to the office'){
+       return `This Monday, I will ${activity}.`;
+    }
+    function wrapAdjective(wrap="*"){
+       
+       const  result=function(emphatic="a hard worker"){
+       
+         return `You are ${wrap}${emphatic}${wrap}!`; 
+       }
+       return result;
+    }
+     saturdayFun("bathe my dog");
+     mondayWork("work ;
